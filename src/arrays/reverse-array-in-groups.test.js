412f3fdd060896ea7fda0084df5b8d3edd986b19
juan ignacio galan <juan.galan@mercadolibre.com>
2018-05-10 10:39:17 -0300

    started working with reverse array in groups exercise

---
+++ src/arrays/reverse-array-in-groups.test.js
@@ -0,0 +1,13 @@
+// Given an array, reverse every sub-array formed by consecutive k elements.
+// https://practice.geeksforgeeks.org/problems/reverse-array-in-groups/0
+
+const reverseArrayInGroups = (list, k) => {
+    
+};
+
+describe('Reverse array in groups', () => {
+    it('test 1', () => {
+        const firstTest = [0, 2, 1, 2, 0];
+        const result = sortList(firstTest);
+    });
+});
